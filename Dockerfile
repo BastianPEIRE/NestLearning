e4593d772d196f3fca4ee724ea7517a4a5723716
Bastian PEIRE <bastian.peire@gmail.com>
2022-06-24 20:40:14 +0200

    docker node + mongo

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,11 @@
 FROM node:latest
 
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+
+RUN npm install
+
+COPY . .
+
+CMD ["npm", "start"]
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { parentPort } from 'worker_threads';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
